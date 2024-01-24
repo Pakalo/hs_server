5fd29757abef2210f062b203abdcb02befcd879a
Pakalo <dorianrochette@gmail.com>
2024-01-24 18:32:09 +0100

    Creation dbSync

---
+++ dbSync.js
@@ -0,0 +1,33 @@
+const User = require('./table/user'); // Importer le modèle User depuis le fichier existant
+const Room = require('./table/room'); // Importer le modèle Room depuis le fichier existant
+const userParty = require('./table/userParty'); // Importer le modèle userParty depuis le fichier existant
+
+
+function dbSync() {
+    User.sync()
+    .then(() => {
+        console.log('✅ Model USER bien synchronisé');
+    })
+    .catch((error) => {
+        console.error('❌ ERREUR syncro model user: ', error);
+    });
+
+    Room.sync({ alter: true })
+  .then(() => {
+    console.log('✅ Model ROOM bien synchronisé')
+  })
+  .catch((error) => {
+    console.error('❌ ERREUR syncro model room: ', error);
+  });
+
+userParty.sync({ alter: true })
+  .then(() => {
+    console.log('✅ Model USERPARTY bien synchronisé');
+  })
+  .catch((error) => {
+    console.error('❌ ERREUR syncro model userParty: ', error);
+  });
+
+}
+
+module.exports = dbSync;
